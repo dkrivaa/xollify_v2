5dc3f74999937e65d6a7d1b88830c3f03655b3af
dkrivaa <dannykrivaa@hotmail.com>
2026-02-25 19:42:15 +0200

    Initial commit

--- database/core/create_db.py
+++ database/core/create_db.py
@@ -1,5 +1,7 @@
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy import inspect
+import os
+from dotenv import load_dotenv
 import asyncio
 from common.db.connection import get_engine, get_session
 from database.core.models import Base
@@ -17,7 +19,10 @@ async def tables_exist(conn) -> bool:
 
 async def create_db():
     """ Create the database and its tables if they do not already exist. """
-    engine = get_engine()
+    load_dotenv()
+    DATABASE_URL = os.getenv('DATABASE_URL')
+
+    engine = get_engine(DATABASE_URL)
     async with engine.begin() as conn:
         # Check if tables already exist
         if await tables_exist(conn):
