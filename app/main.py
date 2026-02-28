e65170d5030e27783fec458f2d7b7ff0c9356f58
dkrivaa <dannykrivaa@hotmail.com>
2026-02-28 16:22:41 +0200

    Initial commit

--- app/main.py
+++ app/main.py
@@ -12,12 +12,16 @@ if sys.platform == 'win32':
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import streamlit as st
 
+from common.bootstrap import initialize_backend
+from common.services.supermarkets import get_chain_from_code
 from backend.services.async_runner import run_async
-from backend.db.crud.items import test_item
+from backend.db.crud.items import item_details
 
 
-
+initialize_backend()
 st.write('hello')
 # data = run_async(test_item, item_code='7290000072753')
 # st.write(data)
+
+st.write(get_chain_from_code('7290027600007'))
 
