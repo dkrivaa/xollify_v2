d7508e28d5e91bdfce2964ed4813c312782fedfd
dkrivaa <dannykrivaa@hotmail.com>
2026-03-05 21:37:21 +0200

    Initial commit

--- app/ui/views/items_section.py
+++ app/ui/views/items_section.py
@@ -13,11 +13,14 @@ def items_section_element():
     # Checks of user selections (stores and home store) and data
     enforce_workflow()
 
+
+
     # Get data for item selector (from home store)
     store = st.session_state.db.get('home_store').get('value', [])
     price_data = data_for_store_from_db(store=store)
     # Display item selector
     item = item_selector(price_data=price_data)
+    do_alternatives = st.checkbox(label='Select alternative item?')
     st.space()
 
     # Show results
@@ -38,30 +41,33 @@ def items_section_element():
             # Default effective item code for this store
             effective_item = item  # default to original
 
-            if not item_dict:
-                # Check if user already selected an alternative for this store
-                alt_key = f"alt_{store_key}_{item}"
-                alt_item_id = st.session_state.db.get(alt_key, {})
-                alt_item = alt_item_id.get('value', None)
+            # Alternative items
+            if do_alternatives:
 
-                if alt_item:
-                    # Use alternative item selected in previous rerun
-                    effective_item = alt_item
-                    item_dict = next((d for d in price_data if d['ItemCode'] == alt_item), {})
-                else:
-                    # Flag to run alternative dialog is raised
-                    flag = st.session_state.db.get(item_id='alternative_flag')
-                    if flag and flag.get('value', False):
-                        # Get item dict from supabase db
-                        original_dict = get_item_dict_from_db(item)
-                        alt_key = f"alt_{store_key}_{item}"
-                        # Show alternative dialog
-                        alternative_dialog(price_data, original_dict, store, alt_key)
-                        st.stop()  # Avoid continued code execution in current run
+                if not item_dict:
+                    # Check if user already selected an alternative for this store
+                    alt_key = f"alt_{store_key}_{item}"
+                    alt_item_id = st.session_state.db.get(alt_key, {})
+                    alt_item = alt_item_id.get('value', None)
+
+                    if alt_item:
+                        # Use alternative item selected in previous rerun
+                        effective_item = alt_item
+                        item_dict = next((d for d in price_data if d['ItemCode'] == alt_item), {})
                     else:
-                        # No alternative yet — raise flag to show alternative dialog
-                        st.session_state.db.put(item_id='alternative_flag', value=True)
-                        st.rerun()
+                        # Flag to run alternative dialog is raised
+                        flag = st.session_state.db.get(item_id='alternative_flag')
+                        if flag and flag.get('value', False):
+                            # Get item dict from supabase db
+                            original_dict = get_item_dict_from_db(item)
+                            alt_key = f"alt_{store_key}_{item}"
+                            # Show alternative dialog
+                            alternative_dialog(price_data, original_dict, store, alt_key)
+                            st.stop()  # Avoid continued code execution in current run
+                        else:
+                            # No alternative yet — raise flag to show alternative dialog
+                            st.session_state.db.put(item_id='alternative_flag', value=True)
+                            st.rerun()
 
             # Display price element
             price_element(item=effective_item, item_details=item_dict, store=store, delta=effective_item != item)
