2b1f7f4be82eb09b48b3a38ebc1074f9b4caaa1b
dkrivaa <dannykrivaa@hotmail.com>
2026-03-06 16:59:10 +0200

    Initial commit

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ initialize_backend()
 
 # st.set_page_config - Set the configuration of the Streamlit page
 st.set_page_config(
-    layout="centered",
+    layout="wide",
     initial_sidebar_state="collapsed"
 )
 
