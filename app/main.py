315f7f5ac583338a6ab7c7314af381dcaceeb42e
dkrivaa <dannykrivaa@hotmail.com>
2026-03-07 10:41:46 +0200

    Initial commit

--- app/main.py
+++ app/main.py
@@ -23,11 +23,6 @@ from typing import Literal
 from common.bootstrap import initialize_backend
 initialize_backend()
 
-# st.set_page_config - Set the configuration of the Streamlit page
-# # Get screen size
-# screen_width = streamlit_js_eval(js_expressions="window.innerWidth", key="sw")
-# # Define layout for mobile ("wide") and desktop ("centered")
-# layout: Literal["centered", "wide"] = "wide" if (screen_width and screen_width < 768) else "centered"
 st.set_page_config(
     layout="centered",
     initial_sidebar_state="collapsed"
@@ -42,7 +37,7 @@ st.write(f'sid: {sid}')
 # Initialize IndexedDB
 from backend.services.indexeddb_session import SessionIndexedDB
 if "db" not in st.session_state:
-    st.session_state.db = SessionIndexedDB(f"XollifyDB_{sid}", "data")
+    st.session_state.db = SessionIndexedDB(f"XollifyDB", "data")
     st.session_state.db.init()
 
 
--- app/ui/views/home.py
+++ app/ui/views/home.py
@@ -25,7 +25,6 @@ def render():
     # Add logo at top of page
     logo()
 
-    # st.write(st.session_state.db.get_all())
     # Navigation menu
     navigation_selection = navigation_section()
     st.space()
