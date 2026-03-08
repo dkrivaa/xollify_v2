5e611d68957395f233de35cd96017b89afab9068
dkrivaa <dannykrivaa@hotmail.com>
2026-03-08 13:22:43 +0200

    Initial commit

--- app/ui/views/lists_section.py
+++ app/ui/views/lists_section.py
@@ -9,7 +9,7 @@ def lists_section_element():
     enforce_workflow()
 
     tab1, tab2, tab3 = st.tabs([":green[:material/upload: Upload List]",
-                                ":green[:material/add_shopping_list: Make/Add to List]",
+                                ":green[:material/add_shopping_cart: Make/Add to List]",
                                 ":green[:material/visibility: See List]"])
 
 
