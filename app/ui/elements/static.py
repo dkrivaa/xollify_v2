8f3e982229047755617a673349c51982d1c1fc89
dkrivaa <dannykrivaa@hotmail.com>
2026-03-08 12:14:07 +0200

    Initial commit

--- app/ui/elements/static.py
+++ app/ui/elements/static.py
@@ -13,7 +13,7 @@ def logo():
 def explanation():
     """ Popover with explanation of app """
     with st.popover(label='Learn more', type='tertiary', ):
-        st.mark
+        st.subheader(body=':yellow[Xollify]', text_alignment='center')
 
 
 def no_stores_selected():
