5f906340eb0bd78b9f950ecc36a67e5436ec41e2
dkrivaa <dannykrivaa@hotmail.com>
2026-03-09 14:24:45 +0200

    Initial commit

--- app/ui/views/lists_section.py
+++ app/ui/views/lists_section.py
@@ -9,9 +9,8 @@ def lists_section_element():
     # Checks of user selections (stores and home store) and data
     enforce_workflow()
 
-    tab1, tab2, tab3 = st.tabs([":green[:material/upload: Upload List]",
-                                ":green[:material/add_shopping_cart: Make/Add to List]",
-                                ":green[:material/visibility: See List]"])
+    tab1, tab2 = st.tabs([":green[:material/upload: Upload List]",
+                                ":green[:material/visibility: Make/Edit List]",])
 
     with tab1:
         # Counter to clear file_upload after file read
