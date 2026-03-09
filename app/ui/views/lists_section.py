a4ce48042f23c3e998ceb39052331c1d9089ce82
dkrivaa <dannykrivaa@hotmail.com>
2026-03-09 14:54:55 +0200

    Initial commit

--- app/ui/views/lists_section.py
+++ app/ui/views/lists_section.py
@@ -31,7 +31,8 @@ def lists_section_element():
 
         # Message
         items_list_exist = st.session_state.db.get(item_id='items_list', default={})
-        if items_list_exist.get['value']:
+        if items_list_exist:
+            items_list = items_list_exist.get['value']
             st.success(f"Shopping list uploaded successfully. Your list has {len(items_list)} items. "
                        f"Go to 'Make/Edit Shopping List' to see list.")
 
