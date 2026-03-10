f8e54ddff9c0aa353bb812d907acfcae64cb96e7
dkrivaa <dannykrivaa@hotmail.com>
2026-03-10 22:56:26 +0200

    Initial commit

--- app/ui/views/lists_section.py
+++ app/ui/views/lists_section.py
@@ -27,12 +27,11 @@ def lists_section_element():
             items_list = read_uploaded_file(uploaded_file)
             # enrich items_list with price and name
             enriched_items_list = enrich_items_list(items_list)
-            st.write(enriched_items_list)
 
-            # # Add to uploader_counter => clear upload widget when rerun
-            # st.session_state.uploader_counter += 1
-            # # Enter items_list into session state and indexedDB
-            # st.session_state.db.put(item_id='items_list', value=items_list)
+            # Add to uploader_counter => clear upload widget when rerun
+            st.session_state.uploader_counter += 1
+            # Enter items_list into session state and indexedDB
+            st.session_state.db.put(item_id='enriched_items_list', value=items_list)
 
 
 
