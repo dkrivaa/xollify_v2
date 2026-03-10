b25dd808bf137918839b5d9c743db2f9ee9fe502
dkrivaa <dannykrivaa@hotmail.com>
2026-03-10 08:33:41 +0200

    Initial commit

--- app/ui/elements/dynamic.py
+++ app/ui/elements/dynamic.py
@@ -181,7 +181,7 @@ def popover_content():
                 return stores[0]
 
             if len(stores) > 1:
-                del st.session_state['home_store']
+                st.session_state.pop("home_store", None)
                 st.subheader(body=':material/home: Select "Home Store"')
                 st.markdown(body='The "Home Store" is used to select items for your shopping '
                                  'and should represent where you normally shop',
