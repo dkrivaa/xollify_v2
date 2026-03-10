a9c9e8e46eeebe81601ef11571e0072fa171b3e3
dkrivaa <dannykrivaa@hotmail.com>
2026-03-10 09:17:54 +0200

    Initial commit

--- app/ui/views/home.py
+++ app/ui/views/home.py
@@ -41,8 +41,7 @@ def render():
         lists_section_element()
 
     stores = st.session_state.db.get(item_id='stores', default={})
-    if stores:
-        st.write(len(stores['value']))
+    st.write(len(stores['value']))
 
 
 def navigation_section():
