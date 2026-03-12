2dff6b1c4b5eb5c25c0372243e031b15c79a2435
dkrivaa <dannykrivaa@hotmail.com>
2026-03-12 08:05:56 +0200

    Initial commit

--- app/ui/utilities/general.py
+++ app/ui/utilities/general.py
@@ -177,6 +177,9 @@ def store_data_for_selected_stores(stores: list[dict]):
                 price_data = run_async(get_stores_price_data, stores=stores_to_fetch),
                 promo_data = run_async(get_stores_promo_data, stores=stores_to_fetch),
 
+            st.write(price_data)
+            st.stop()
+
             # Enter final data into session state and indexedDB
             if price_data:
                 st.session_state.db.put_many([
