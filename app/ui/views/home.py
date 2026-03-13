81f8bcc702ad15426d3ff0fd497926633f0a8dd4
dkrivaa <dannykrivaa@hotmail.com>
2026-03-13 20:30:49 +0200

    Initial commit

--- app/ui/views/home.py
+++ app/ui/views/home.py
@@ -8,7 +8,7 @@ def render():
     """ Main func to render page """
     logo()
     language, icon_position = lang()
-    st.divider()
+    st.space()
 
     label1 = {'english': 'Product Price & Promos',
               'hebrew': 'מחיר מוצר ומבצעים'}
