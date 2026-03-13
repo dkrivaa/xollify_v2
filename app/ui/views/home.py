ebe27d13da55390af179c46a026afc4fb674fffa
dkrivaa <dannykrivaa@hotmail.com>
2026-03-13 16:30:01 +0200

    Initial commit

--- app/ui/views/home.py
+++ app/ui/views/home.py
@@ -5,7 +5,6 @@ from ui.elements.static import logo
 
 def render():
     """ Main func to render page """
-    st.logo(image='https://www.google.com/imgres?q=shufersal&imgurl=https%3A%2F%2Fmma.prnewswire.com%2Fmedia%2F1592092%2FShufersal_Logo.jpg%3Fp%3Dfacebook&imgrefurl=https%3A%2F%2Fwww.prnewswire.com%2Fil%2Fnews-releases%2Fshufersal-reports-second-quarter-and-first-half-2021-financial-results-301353115.html&docid=e5BSHVC6CfRxwM&tbnid=Ot0TNKJaiy8BnM&vet=12ahUKEwiax5TZip2TAxXBg_0HHdxtHlkQnPAOegQIGxAB..i&w=2700&h=1414&hcb=2&ved=2ahUKEwiax5TZip2TAxXBg_0HHdxtHlkQnPAOegQIGxAB')
     logo()
 
     st.button(label='Test',
