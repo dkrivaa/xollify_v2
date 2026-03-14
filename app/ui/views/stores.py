7e027c1bc0bff9075f6450fbcbfd8b9a73a5c968
dkrivaa <dannykrivaa@hotmail.com>
2026-03-14 10:25:52 +0200

    Initial commit

--- app/ui/views/stores.py
+++ app/ui/views/stores.py
@@ -20,6 +20,35 @@ def render():
         if chain_code:
             # Show store selector for selected chain
             store_code, store_name = store_selector(chain_code)
+            # if store_code:
+            # Add store to session_state
+            if st.button(label='Add Store',
+                         icon=':material/add_business:',
+                         icon_position='left',
+                         width='stretch',
+                         key='add_store_button',
+                         disabled=not store_code):
+                # Enter new store into session_state and indexedDB
+                try:
+                    current = st.session_state.db.get(item_id='stores')['value']
+                    if not current:
+                        current = []
+                except TypeError as e:
+                    # TypeError -> current is None =>
+                    current = []
+                # Add new store if not in list already
+                new_store = {'chain_code': chain_code,
+                             'chain_alias': chain_alias,
+                             'store_code': store_code,
+                             'store_name': store_name}
+                if new_store not in current:
+                    current.append(new_store)
+                    # Enter all stores into session_state and indexedDB
+                    st.session_state.db.put(item_id='stores', value=current)
+                    # Delete present home store
+                    st.session_state.db.delete(item_id='home_store')
+                    # Reset flag to clear select boxes
+                    st.session_state['reset_selectors_flag'] = True
 
 
 
