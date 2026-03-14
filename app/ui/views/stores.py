6916aa5b312a88d1bd30a0743bf8383fda873216
dkrivaa <dannykrivaa@hotmail.com>
2026-03-14 09:16:06 +0200

    Initial commit

--- app/ui/views/stores.py
+++ app/ui/views/stores.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 from ui.elements.static import logo
 from ui.elements.dynamic import lang
+from ui.views.stores_section import stores_section_element
 
 
 def render():
@@ -9,6 +10,8 @@ def render():
     logo()
     st.divider()
     st.space()
+
+    stores_section_element()
 
 
 
