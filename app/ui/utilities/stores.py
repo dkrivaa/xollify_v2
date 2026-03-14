130c8c446c653ac0e3300e4148494de22887e160
dkrivaa <dannykrivaa@hotmail.com>
2026-03-14 10:43:06 +0200

    Initial commit

---
+++ app/ui/utilities/stores.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+
+def add_store_to_session_state_indexeddb(chain_code: str,
+                                         chain_alias: str,
+                                         store_code: str,
+                                         store_name: str,
+                                         home_store: bool = False):
+    """ Function to enter selected store into session_state and indexedDB """
+    if home_store:
+        store = {'chain_code': chain_code,
+                 'chain_alias': chain_alias,
+                 'store_code': store_code,
+                 'store_name': store_name}
+        st.session_state.db.put(item_id='home_store', value=store)
+
+    else:
+        try:
+            current = st.session_state.db.get(item_id='stores')['value']
+            if not current:
+                current = []
+        except TypeError as e:
+            # TypeError -> current is None =>
+            current = []
+        # Add new store if not in list already
+        new_store = {'chain_code': chain_code,
+                     'chain_alias': chain_alias,
+                     'store_code': store_code,
+                     'store_name': store_name}
+        if new_store not in current:
+            current.append(new_store)
+            # Enter all stores into session_state and indexedDB
+            st.session_state.db.put(item_id='stores', value=current)
--- app/ui/views/stores.py
+++ app/ui/views/stores.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from ui.elements.static import logo
 from ui.elements.dynamic import chain_selector, store_selector, lang
-from ui.views.stores_section import stores_section_element
+from ui.utilities.stores import add_store_to_session_state_indexeddb
 
 
 def render():
@@ -14,43 +14,22 @@ def render():
     with st.chat_message(name='ai', width='stretch'):
         st.markdown(body='Just a few questions:')
 
-        st.markdown(body='Where do you normally shop?')
+        st.markdown(body=':blue[Where do you normally shop?]')
         # Show chain selector
         chain_code, chain_alias = chain_selector()
         if chain_code:
             # Show store selector for selected chain
             store_code, store_name = store_selector(chain_code)
-            # if store_code:
             # Add store to session_state
-            if st.button(label='Add Store',
+            if st.button(label='Add Home Store',
                          icon=':material/add_business:',
                          icon_position='left',
                          width='stretch',
                          key='add_store_button',
                          disabled=not store_code):
                 # Enter new store into session_state and indexedDB
-                try:
-                    current = st.session_state.db.get(item_id='stores')['value']
-                    if not current:
-                        current = []
-                except TypeError as e:
-                    # TypeError -> current is None =>
-                    current = []
-                # Add new store if not in list already
-                new_store = {'chain_code': chain_code,
-                             'chain_alias': chain_alias,
-                             'store_code': store_code,
-                             'store_name': store_name}
-                if new_store not in current:
-                    current.append(new_store)
-                    # Enter all stores into session_state and indexedDB
-                    st.session_state.db.put(item_id='stores', value=current)
-                    # Delete present home store
-                    st.session_state.db.delete(item_id='home_store')
-                    # Reset flag to clear select boxes
-                    st.session_state['reset_selectors_flag'] = True
-
-
+                add_store_to_session_state_indexeddb(chain_code, chain_alias, store_code,
+                                                     store_name, home_store=True)
 
 
 
