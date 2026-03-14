1517c26d76070e2b3d78af050f42ef8138aa411c
dkrivaa <dannykrivaa@hotmail.com>
2026-03-14 21:40:36 +0200

    Initial commit

--- app/ui/utilities/stores.py
+++ app/ui/utilities/stores.py
@@ -14,20 +14,19 @@ def add_store_to_session_state_indexeddb(chain_code: str,
                  'store_name': store_name}
         st.session_state.db.put(item_id='home_store', value=store)
 
-    else:
-        try:
-            current = st.session_state.db.get(item_id='stores')['value']
-            if not current:
-                current = []
-        except TypeError as e:
-            # TypeError -> current is None =>
+    try:
+        current = st.session_state.db.get(item_id='stores')['value']
+        if not current:
             current = []
-        # Add new store if not in list already
-        new_store = {'chain_code': chain_code,
-                     'chain_alias': chain_alias,
-                     'store_code': store_code,
-                     'store_name': store_name}
-        if new_store not in current:
-            current.append(new_store)
-            # Enter all stores into session_state and indexedDB
-            st.session_state.db.put(item_id='stores', value=current)
+    except TypeError as e:
+        # TypeError -> current is None =>
+        current = []
+    # Add new store if not in list already
+    new_store = {'chain_code': chain_code,
+                 'chain_alias': chain_alias,
+                 'store_code': store_code,
+                 'store_name': store_name}
+    if new_store not in current:
+        current.append(new_store)
+        # Enter all stores into session_state and indexedDB
+        st.session_state.db.put(item_id='stores', value=current)
