d9d5c9a317a43b394039d5cbf85f7ec665c90664
dkrivaa <dannykrivaa@hotmail.com>
2026-03-14 10:11:11 +0200

    Initial commit

--- app/ui/views/stores.py
+++ app/ui/views/stores.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 from ui.elements.static import logo
-from ui.elements.dynamic import lang
+from ui.elements.dynamic import chain_selector, lang
 from ui.views.stores_section import stores_section_element
 
 
@@ -13,6 +13,10 @@ def render():
 
     with st.chat_message(name='ai', width='stretch'):
         st.markdown(body='Just a few questions:')
+
+        st.markdown(body='Where do you normally shop?')
+        # Show chain selector
+        chain_code, chain_alias = chain_selector()
 
 
 
