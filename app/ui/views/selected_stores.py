39316b5110818723eef1dfe1f69d51b87a1990ad
dkrivaa <dannykrivaa@hotmail.com>
2026-03-15 07:14:12 +0200

    Initial commit

---
+++ app/ui/views/selected_stores.py
@@ -0,0 +1,53 @@
+import streamlit as st
+
+from ui.utilities.general import remove_home_store_from_db
+
+
+def make_data_for_editor(data: list[dict]):
+    """ Make the data ready for insert into data_editor by adding delete column """
+    return [{**d, 'delete': False, } for d in data]
+
+
+def reorganize_data(edited_data: list[dict]):
+    """ Reorganize data after edit by user """
+    keys_to_remove = ["delete", ]
+    # Remove keys and remove rows with delete=True
+    stores = [
+        {k: v for k, v in d.items() if k not in keys_to_remove}
+        for d in edited_data
+        if not d.get("delete", False)
+    ]
+    # Remove home store if removed from selected stores
+    remove_home_store_from_db(stores=stores)
+    # Save updated stores data to session_state and indexedDB
+    st.session_state.db.put(item_id='stores', value=stores)
+    # Rerun app
+    st.rerun()
+
+
+def render():
+    """ Page to manage selected stores. Main function to render page """
+    data = st.session_state.db.get(item_id='stores')
+
+    if data:
+        organized_data = make_data_for_editor(data.get('value'))
+        edited_data = st.data_editor(
+            data=organized_data, width='stretch',
+            column_order=['chain_alias', 'store_name', 'delete'],
+            column_config={'chain_alias': st.column_config.TextColumn(label='Chain', disabled=True),
+                           'store_name': st.column_config.TextColumn(label='Store Name', disabled=True),
+                           'chain_code': st.column_config.TextColumn(label='Chain Code'),
+                           'store_code': st.column_config.TextColumn(label='Store'),
+                           'delete': st.column_config.CheckboxColumn(label='Delete', width='small', )
+                           }
+        )
+
+        if st.button(label='Update',
+                     icon=':material/refresh:',
+                     icon_position='left',
+                     width='stretch'):
+            reorganize_data(edited_data)
+
+
+if __name__ == "__main__":
+    render()
