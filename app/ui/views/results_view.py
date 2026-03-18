3cad5f56f99d6364396c353a7a4c09f790fbaefe
dkrivaa <dannykrivaa@hotmail.com>
2026-03-18 17:13:16 +0200

    Initial commit

--- app/ui/views/results_view.py
+++ app/ui/views/results_view.py
@@ -59,7 +59,7 @@ def render():
 
         st.metric(label="Total Cost:",
                   value=f"₪ {best_total:.2f}",
-                  delta="" if best_total - best_total_1 == 0 else f"₪ {(best_total - best_total_1):.2f}",
+                  delta=f"₪ {(best_total - best_total_1):.2f}" if best_total - best_total_1 != 0 else "",
                   delta_color='inverse',
                   width='stretch')
 
