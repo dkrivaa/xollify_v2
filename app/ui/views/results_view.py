cf9bc4f07f61faa0e3ada465a4e2ebbf2aa54e3c
dkrivaa <dannykrivaa@hotmail.com>
2026-03-19 08:31:18 +0200

    Initial commit

--- app/ui/views/results_view.py
+++ app/ui/views/results_view.py
@@ -28,6 +28,9 @@ def render():
         for store in stores:
             total = total_cost_per_store(shoppinglists, store)
 
+            st.write(float(total))
+            st.write(float(best_total_1))
+
             st.metric(label=f":blue[{store['chain_alias']} - {store['store_name']}]",
                       value=f"₪ {total:.2f}",
                       delta="" if float(total) == float(best_total_1) else f"₪ {(total - best_total_1):.2f}",
